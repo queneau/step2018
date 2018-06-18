66368b9aa8a33708e35136bc8e3685cd1366bcea
Risako Kuno <risakno3@gmail.com>
2018-06-18 23:50:21 +0900

    fix reanNumber

--- hw03/calculator_2.py
+++ hw03/calculator_2.py
@@ -1,17 +1,17 @@
 #数式を行で読み込み
 def readNumber(line, index):
     number = 0
-    while index < len(line) and line[index].isdigit():
-        number = number * 10 + int(line[index])
+    flag = 0
+    figure = 1
+    while index < len(line) and (line[index].isdigit() or line[index] == '.'):
+        if line[index] = = '.':
+            flag = 1
+        else:
+            number = number * 10 + int(line[index])
+            if flag == 1:
+                figure *= 0.1 
         index += 1
-    if index < len(line) and line[index] == '.':
-        index += 1
-        keta = 0.1
-        while index < len(line) and line[index].isdigit():
-            number += int(line[index]) * keta
-            keta *= 0.1
-            index += 1
-    token = {'type': 'NUMBER', 'number': number}
+    token = {'type': 'NUMBER', 'number': number*figure}        
     return token, index
 
 
